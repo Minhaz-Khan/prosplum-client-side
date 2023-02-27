ce7660f916cc171c89bc94072f77f84f112433c2
Minhaz-Khan <kminhaz111@gmail.com>
2023-02-27 17:48:34 +0600

    footer section done

--- src/Component/Pages/Services/Services.js
+++ src/Component/Pages/Services/Services.js
@@ -5,7 +5,7 @@ import Service from '../Service/Service';
 const Services = () => {
     const services = useLoaderData();
     return (
-        <div className='container mx-auto'>
+        <div className='container mx-auto '>
             <h5 className='text-center text-sm font-bold text-blue-600 mt-6'>OUR SERVICES</h5>
             <h2 className='text-center text-5xl font-bold mt-2'>Our Plumming Services</h2>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-10'>
